f42ebc3c83fda85606294316faec490b2c4e443d
Jakub-Nnoli <126829090+Jakub-Nnoli@users.noreply.github.com>
2023-06-19 17:17:33 +0200

    Delete database if necessary update

--- install.bat
+++ install.bat
@@ -2,5 +2,6 @@ git clone https://github.com/Jakub-Nnoli/NoteAppProject.git
 cd NoteAppProject
 del install.bat
 del README.md
+del NotepadDatabase.db
 pip install -r requirements.txt
 python login_window.py
