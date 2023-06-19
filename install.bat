0a129fa45447c2206c4d21da84185f3deb588354
Jakub-Nnoli <jmnnoli@gmail.com>
2023-06-19 17:19:07 +0200

    final touch for project (i hope)

--- install.bat
+++ install.bat
@@ -2,6 +2,5 @@ git clone https://github.com/Jakub-Nnoli/NoteAppProject.git
 cd NoteAppProject
 del install.bat
 del README.md
-del NotepadDatabase.db
 pip install -r requirements.txt
 python login_window.py
