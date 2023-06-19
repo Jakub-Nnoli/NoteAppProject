64b8f73c71eb000c069c0931269a635d60d614c7
Jakub-Nnoli <126829090+Jakub-Nnoli@users.noreply.github.com>
2023-06-19 17:36:42 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,5 +2,7 @@ git clone https://github.com/Jakub-Nnoli/NoteAppProject.git
 cd NoteAppProject
 del install.bat
 del README.md
+rmdir .git
+rmdir __pycache__
 pip install -r requirements.txt
 call NoteApp.bat
