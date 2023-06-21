309402fdf88841ee11dfa16ae406277811d3fdf3
Jakub-Nnoli <126829090+Jakub-Nnoli@users.noreply.github.com>
2023-06-21 10:08:25 +0200

    hotfix (wrong shortcut assignment)

--- user_notes_window.py
+++ user_notes_window.py
@@ -61,7 +61,7 @@ class UserNotesWindow(QMainWindow):
         self.deleteButton.setText("Delete")
         self.deleteButton.setIcon(QIcon(os.path.join(self.iconPath, "DeleteIcon.png")))
         self.deleteButton.clicked.connect(self.deleteSelectedNote)
-        self.deleteAccount.setShortcut(QKeySequence(Qt.Key.Key_Delete))
+        self.deleteButton.setShortcut(QKeySequence(Qt.Key.Key_Delete))
 
         self.menubar = QMenuBar()
         self.menubar.setGeometry(QRect(0, 0, 800, 22))
